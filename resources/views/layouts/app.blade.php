b94d22bcb7451ea17caa334fee905641f279d3d6
rocky <rockyueno@gmail.com>
2021-10-22 15:17:29 +0000

    user registration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,10 @@
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
                     <ul class="navbar-nav">
-                        {{-- タスク作成ページへのリンク --}}
-                        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                        {{{-- ユーザ登録ページへのリンク --}}
+                        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                        {{-- ログインページへのリンク --}}
+                        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
                     </ul>
                 </div>
             </nav>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@
 
 Route::get('/', 'TasksController@index');
 
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::resource('tasks', 'TasksController');
